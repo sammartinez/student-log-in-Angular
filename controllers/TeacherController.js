dae82e6f9ca5358f54cb43859c3a79997505d7b4
Sam Martinez <sammartinez19@gmail.com>
2015-09-25 15:57:56 -0700

    verify everything in working

--- controllers/TeacherController.js
+++ controllers/TeacherController.js
@@ -1,5 +1,6 @@
 studentLogin.controller('TeacherCtrl', function TeacherCtrl($scope, StudentsFactory) {
 
+    //No methods for teachers. Can only see who is here and who is not.
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
   });
